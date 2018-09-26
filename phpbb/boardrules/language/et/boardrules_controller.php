17e284ef48e1502a79728cf552a77feebb58a4c6
phpBBEesti <argol76@gmail.com>
2018-09-26 12:11:04 +0300

    Update boardrules_controller.php

--- phpbb/boardrules/language/et/boardrules_controller.php
+++ phpbb/boardrules/language/et/boardrules_controller.php
@@ -2,7 +2,7 @@
 /**
 *
 * Board Rules extension for the phpBB Forum Software package.
-* Estonian translation by phpBBeesti.com <http://www.phpbbeesti.com>
+* Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
 *
 * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
